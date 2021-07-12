6f677bfa5004907d1697f0be0fe9a44ab9b75f4a
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-07-12 17:36:22 +0500

    delete file test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './app/App.jsx';
-
-test('render App(not auth user) ', () => {
-  render(<App />);
-  screen.debug();
-});
