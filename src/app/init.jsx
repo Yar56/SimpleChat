78c7842ccae4a2046ce63e43370f1c51e54b847b
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-07-12 17:44:31 +0500

    fix2 index.js

---
+++ src/app/init.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import i18nInstance from './i18n.js';
+import {
+  setActiveChannel,
+  removeChannel,
+  renameChannel,
+  addChannel,
+} from '../features/channels/channelsSlice.js';
+import { addMessage } from '../features/messages/messagesSlice.js';
+
+import store from '../store/index.js';
+import App from './App.jsx';
+
+const init = (socket) => {
+  socket.on('newMessage', (message) => {
+    store.dispatch(addMessage(message));
+  });
+  socket.on('newChannel', (data) => {
+    store.dispatch(setActiveChannel({ id: data.id }));
+    store.dispatch(addChannel(data));
+  });
+  socket.on('removeChannel', ({ id: channelId }) => {
+    store.dispatch(removeChannel({ channelId }));
+    // TODO: подумать над дефолтным id
+    store.dispatch(setActiveChannel({ id: 1 }));
+  });
+  socket.on('renameChannel', (response) => {
+    store.dispatch(renameChannel({ id: response.id, name: response.name }));
+  });
+
+  const vdom = (
+
+    <Provider store={store}>
+      <I18nextProvider i18n={i18nInstance}>
+        <App socket={socket} />
+      </I18nextProvider>
+    </Provider>
+
+  );
+
+  return vdom;
+};
+export default init;
